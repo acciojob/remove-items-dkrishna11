4f6f2950912bc609bde076b987d81f652d4d3eb3
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-26 15:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
 let select=document.getElementsById("colorSelect");
-let select=document.getElementsById("colorSelect");
+let button=document.getElementsByTagName("input")[0];
