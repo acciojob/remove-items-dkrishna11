19a2dd2831f77942a218d08e2b85737a3318999f
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-26 15:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let select=document.getElementsById("colorSelect");
 let button=document.getElementsByTagName("input")[0];
 
 function remove(){
-	if(select.value==)
+	if(select.value==="Red")
+		select.value="";
 }
+
+button.createEventListener(click, remove);
